48b2068e37b4574a2d5445e6708df401698f7667
mixnuts <french0719@gmail.com>
2023-10-22 00:02:36 +0900

    add tailwind and daisyui

--- src/popup/App.tsx
+++ src/popup/App.tsx
@@ -1,3 +1,10 @@
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className="underline text-teal-200 font-bold">Gather</h1>
+      <h1>Github</h1>
+      <h3>SPEED</h3>
+      <button className="btn">Button</button>
+    </div>
+  );
 }
